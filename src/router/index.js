68513dbeca863301382e2998b23d35209576ec7a
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-09-07 12:42:22 +0700

    layout and ux/ui login page

--- src/router/index.js
+++ src/router/index.js
@@ -2,40 +2,35 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
-
-
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Login",
+    component: () => import("../views/Login.vue"),
   },
   {
-    path: "/login",
-    name: "Login",
-    component: () =>
-      import("../views/Login.vue")
+    path: "/Home",
+    name: "Home",
+    component: Home,
   },
   {
     path: "/admin-stu",
     name: "AdminStudent",
-    component: () =>
-      import("../views/AdminStudent.vue")
+    component: () => import("../views/AdminStudent.vue"),
   },
   {
     path: "/admin-tch",
     name: "AdminTch",
-    component: () =>
-      import("../views/AdminTch.vue")
+    component: () => import("../views/AdminTch.vue"),
   },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
